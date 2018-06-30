f7cd067e8a3d9577c1b98dca961eac5ac57a8033
Joshua Ansari <joshua.ansari@teridiumlabs.com>
2018-06-30 14:20:10 -0700

    Puzzled Penguins Open Source Edition 1.1

---
+++ SlideUnity/Assets/Scripts/SlideUnity/UI/OptionsScreen.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+namespace SlideUnity.UI
+{
+	public class OptionsScreen : UIScreen
+	{
+		[SerializeField]
+		private ToggleSettingsButton ToggleParticlesEnabledButton;
+
+		public override void InitializeScreen()
+		{
+			base.InitializeScreen();
+
+			ToggleParticlesEnabledButton.SetOnClick(() =>
+			{
+				GameManager.Settings.ToggleParticlesEnabled();
+				ToggleParticlesEnabledButton.UpdateSetting(GameManager.Settings.ParticlesEnabled);
+				ToggleParticlesEnabledButton.SetText(GameManager.Settings.ParticlesEnabled ? "Disable Snow" : "Enable Snow");
+			});
+		}
+
+		public override void ShowScreen()
+		{
+			base.ShowScreen();
+
+			ToggleParticlesEnabledButton.UpdateSetting(GameManager.Settings.ParticlesEnabled);
+			ToggleParticlesEnabledButton.SetText(GameManager.Settings.ParticlesEnabled ? "Disable Snow" : "Enable Snow");
+		}
+	}
+}
---
+++ SlideUnity/Assets/Scripts/SlideUnity/UI/UIScreen.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+namespace SlideUnity.UI
+{
+	[RequireComponent(typeof(Canvas))]
+	public class UIScreen : MonoBehaviour
+	{
+		private Canvas _canvas;
+		protected Canvas GetCanvas() => _canvas ?? (_canvas = GetComponent<Canvas>());
+
+		protected void OnEnable()
+		{
+			InitializeScreen();
+		}
+
+		public virtual void InitializeScreen() { }
+
+		public virtual void ShowScreen()
+		{
+			GetCanvas().enabled = true;
+		}
+
+		public virtual void HideScreen()
+		{
+			GetCanvas().enabled = false;
+		}
+	}
+}
